84e0c8a2c768257d3930831a07dda737837ba4fa
Stanislav <satsakul@yandex-team.ru>
2019-01-29 22:41:10 +0300

    master: add fabric

--- app/src/main/java/githubusernamefortesting/com/myapplication/MainActivity.java
+++ app/src/main/java/githubusernamefortesting/com/myapplication/MainActivity.java
@@ -3,8 +3,10 @@ package githubusernamefortesting.com.myapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.crashlytics.android.Crashlytics;
 import com.microsoft.appcenter.AppCenter;
 import com.microsoft.appcenter.distribute.Distribute;
+import io.fabric.sdk.android.Fabric;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_main);
 
         AppCenter.start(getApplication(), APP_CENTER, Distribute.class);
