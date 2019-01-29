c28d5e67d8add5f58bb1ccf2edb4475ccbda5db6
Stanislav <satsakul@yandex-team.ru>
2019-01-29 19:12:04 +0300

    master: start App Center

--- app/src/main/java/githubusernamefortesting/com/myapplication/MainActivity.java
+++ app/src/main/java/githubusernamefortesting/com/myapplication/MainActivity.java
@@ -3,11 +3,18 @@ package githubusernamefortesting.com.myapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.microsoft.appcenter.AppCenter;
+import com.microsoft.appcenter.distribute.Distribute;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final String APP_CENTER = "bf77df9c-ceef-4a6e-b6af-76139c894d5e";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AppCenter.start(getApplication(), APP_CENTER, Distribute.class);
     }
 }
